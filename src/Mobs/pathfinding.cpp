fdf18e87b2559e95168b2f3d0d15808c0f3d410f
Wiseoldman95 <@>
2014-09-08 21:53:32 +0300

    Initial pathfinding commit - doesn't even work

---
+++ src/Mobs/pathfinding.cpp
@@ -0,0 +1,19 @@
+/*
+ /*I AM JUST PLAYING AROUND WITH SOME PSEUDO-CODE, DO NOT USE
+ * THIS BRANCH FOR ANYTHING WHATSOEVER, IT PROBABLY DOES NOT EVEN COMPILE*/
+
+
+
+/* pathfinding.cpp
+ *
+ *  Created on: Sep 8, 2014
+ *      Author: Wiseoldman95
+ *      Implementing Pathfinding, see the following links for info:
+ *      http://forum.mc-server.org/showthread.php?tid=1571
+ *      https://github.com/mc-server/MCServer/issues/1391
+ *
+ */
+
+
+
+
---
+++ src/Mobs/pathfinding.h
@@ -0,0 +1,17 @@
+/*
+/*I AM JUST PLAYING AROUND WITH SOME PSEUDO-CODE, DO NOT USE
+ * THIS BRANCH FOR ANYTHING WHATSOEVER, IT PROBABLY DOES NOT EVEN COMPILE*/
+
+/* pathfinding.h
+ *
+ *  Created on: Sep 8, 2014
+ *      Author: Wiseoldman95
+ */
+
+#ifndef PATHFINDING_H_
+#define PATHFINDING_H_
+
+
+
+
+#endif /* PATHFINDING_H_ */
