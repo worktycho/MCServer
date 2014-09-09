a55667a6c7341de5462bf5ce6399e0b4f8475e2a
Wiseoldman95 <@>
2014-09-09 13:58:18 +0300

    Initial header

--- src/Mobs/pathfinding.h
+++ src/Mobs/pathfinding.h
@@ -1,5 +1,5 @@
  /*I AM JUST PLAYING AROUND WITH SOME PSEUDO-CODE, DO NOT USE
- * THIS BRANCH FOR ANYTHING WHATSOEVER, IT'S PSEUDO C AND
+ * THIS BRANCH FOR ANYTHING WHATSOEVER
  * IT PROBABLY DOES NOT EVEN COMPILE YET*/
 
 /* pathfinding.cpp
@@ -13,13 +13,42 @@
  */
 
 
-
+/*TODO
+ * Get feedback regarding header before implementing
+ * Modify the header to follow the project's code conventions
+ * */
 
 
 #ifndef PATHFINDING_H_
 #define PATHFINDING_H_
 
-typedef struct path_point_t * path_point;
+class cPathfinder
+{
+
+/*
+ * boundingBoxWidth - The bounding box width of the mob wishing to use this Pathfinder
+ * boundingBoxHeight - The bounding box height of the mob wishing to use this Pathfinder
+ * maxUp - How many vertical blocks can this mob move upwards?
+ * 			(e.g. Silverfish: 0, Zombie: 1, Spider: ~20)
+ * maxDown - How many vertical blocks can this mob move downwards?
+ * 		(e.g. Silverfish: 1, Zombie: 1, Spider: ~3?)
+ * maxDistance - The maximum distance allowed for the Pathfinder
+ * When calling moveToByPath, an exception (TODO) will be thrown if startingPoint
+ * is outside an imaginry cube which has an edge the size of maxDistance and its
+ * origin is at endingPoint */
+cPathfinder(double boundingBoxWidth, double boundingBoxHeight,
+		int maxUp,int maxDown , int maxDistance);
 
 
+/*Typical usage: (Pseudocode)
+ * Cmonster::MoveToPosition(myPathfinder.moveToByPath(my_position,player_position));
+ *
+ * Note that this function does not necessarily calculate a new path: If the mob is
+ * far enough a new path will only be re-calculated once every many calls.
+ * Therefore, it is safe to call this every tick */
+const Vector3d & moveToByPath(const Vector3d & currentPoint, const Vector3d & endingPoint);
+
+
+
+};
 #endif /* PATHFINDING_H_ */
